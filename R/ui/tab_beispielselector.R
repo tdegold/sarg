cc452b0b8f6b17d689e703f41270ad8f98b42572
tdegold <timdegold@gmx.net>
2022-01-20 11:18:02 +0100

    added ui elements for Maxima settings

--- R/ui/tab_beispielselector.R
+++ R/ui/tab_beispielselector.R
@@ -4,7 +4,10 @@ tab_beispielselector <- tabItem(tabName = "tab_beispielselector",
     column(4, selectInput("examClass", label = "Jahrgang", c("1xHIT", "2xHIT", "3xHIT", "4xHIT", "5xHIT")),
            textInput("examName", label = "Name der Schularbeit", value = "x. Schularbeit AM"),
            textInput("examKomp", label = "Kompetenzbereich", value = "S03B: Trigonometrie")),
-    column(4, dateInput("examDate", label = "Datum der Schularbeit", value = Sys.Date(), format = "dd. mm. yyyy", language = "de"))
+    column(4, dateInput("examDate", label = "Datum der Schularbeit", value = Sys.Date(), format = "dd. mm. yyyy", language = "de"),
+           strong("Maxima-Einstellungen"),
+           checkboxInput("examInclMaxima", label = "Schularbeit mit Maxima?"),
+           checkboxInput("examSplitMaxima", label = "Maxima-Teil separat?"))
   ),
   DT::dataTableOutput("examplesChosenTable"),
   uiOutput("infoSection"),
