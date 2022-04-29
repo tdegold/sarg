9e67c46d9c72bda7398a599fffcf095daffbaff7
tdegold <timdegold@gmx.net>
2022-04-29 07:40:26 +0200

    ui changes

--- R/ui/tab_beispielselector_sa.R
+++ R/ui/tab_beispielselector_sa.R
@@ -1,17 +1,25 @@
 tab_beispielselector_sa <- tabItem(tabName = "tab_beispielselector_sa",
   fluidRow(
-    column(4, uiOutput("exampleSelector")),
-    column(4, selectInput("examClass", label = "Jahrgang", c("1xHIT", "2xHIT", "3xHIT", "4xHIT", "5xHIT")),
-           textInput("examName", label = "Name der Schularbeit", value = "x. Schularbeit AM"),
-           textInput("examKomp", label = "Kompetenzbereich", value = "S03B: Trigonometrie")),
-    column(4, dateInput("examDate", label = "Datum der Schularbeit", value = Sys.Date(), format = "dd. mm. yyyy", language = "de"),
-           strong("Maxima-Einstellungen"),
-           checkboxInput("examSplitMaxima", label = "Maxima-Teil separat?"))
+    box(title = "Eingabe", status = "primary", width = 12,
+      column(width = 4, uiOutput("exampleSelector")),
+      column(width = 4, selectInput("examClass", label = "Jahrgang", c("1xHIT", "2xHIT", "3xHIT", "4xHIT", "5xHIT")),
+             textInput("examName", label = "Name der Schularbeit", value = "x. Schularbeit AM"),
+             textInput("examKomp", label = "Kompetenzbereich", value = "S03B: Trigonometrie")),
+      column(width = 4, dateInput("examDate", label = "Datum der Schularbeit", value = Sys.Date(), format = "dd. mm. yyyy", language = "de"),
+             strong("Maxima-Einstellungen"),
+             checkboxInput("examSplitMaxima", label = "Maxima-Teil separat?"))
+    ),
   ),
-  DT::dataTableOutput("examplesChosenTable"),
-  uiOutput("infoSection"),
   fluidRow(
-    column(12, 
+    column(width = 12,
+      DT::dataTableOutput("examplesChosenTable")
+    )
+  ),
+  fluidRow(
+     uiOutput("infoSection")
+  ),
+  fluidRow(
+    column(12,
            actionButton("generatePDF", "PDF generieren", class = "btn-success"),
            actionButton("generateMOODLE", "Moodle-XML generieren", class = "btn-success")
     )
